134cef1f76a17f7ccda06d9ac64327fb5ad3902f
corporate <agencystest101@gmail.com>
2024-07-18 17:24:26 -0400

    Update and rename yallow to Teleport To Boss Battle.lua

---
+++ Teleport To Boss Battle.lua
@@ -0,0 +1,4 @@
+-- this script allows you to teleport to the uncanny boss fight without using a uncanny key. please credit "JOJO"/me for making this if you use it!
+local TeleportService = game:GetService("TeleportService")
+local plr = game.Players.LocalPlayer
+TeleportService:Teleport(11423379012)
